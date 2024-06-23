986ae06a0c70dcee9ea15eaa26f4b8578f949f6d
yassine128 <yassineseddaoui@gmail.com>
2024-06-23 13:05:45 -0400

    YS | finished Lane Detection

--- Lane_Detection/opt_image_processing.py
+++ Lane_Detection/opt_image_processing.py
@@ -1,35 +1,69 @@
 import cv2
 import numpy as np 
 import matplotlib.pyplot as plt
-import time
 
 class LaneDetection: 
     def new_img(self, img): 
-        self.img = np.copy(img)
+        self.img = img
+        self.original = self.img
+        self.img = np.copy(self.img)
+        self.preprocess()
+
+    def adjust_gamma(self, image, gamma): 
+        invGamma = 1.0 / gamma
+        table = np.array([((i / 255.0) ** invGamma) * 255
+            for i in np.arange(0, 256)]).astype("uint8")
+        return cv2.LUT(image, table)
+
+    def isolate_color_mask(self, img, low_thresh, high_thresh):
+        return cv2.inRange(img, low_thresh, high_thresh)
+    
+    def to_hls(self, img):
+        return cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+
+    def preprocess(self): 
+        darkened_img = self.adjust_gamma(self.img, 0.5)
+        white_mask = self.isolate_color_mask(self.to_hls(self.img), np.array([0, 200, 0], dtype=np.uint8), np.array([200, 255, 255], dtype=np.uint8))
+        yellow_mask = self.isolate_color_mask(self.to_hls(self.img), np.array([10, 0, 100], dtype=np.uint8), np.array([40, 255, 255], dtype=np.uint8))
+        mask = cv2.bitwise_or(white_mask, yellow_mask)
+        self.img = cv2.bitwise_and(darkened_img, darkened_img, mask=mask)
+        self.smooth()
+
+    def darken_image(self):
+        self.img = cv2.convertScaleAbs(self.img, alpha=0.5, beta=0) 
         self.smooth()
 
     def smooth(self): 
-        self.img = cv2.GaussianBlur(self.img, (5,5), 0)
-        self.canny()
+        self.img = cv2.GaussianBlur(self.img, (3,3), 0)
+        self.canny_func()
         
-    def canny(self): 
+    def canny_func(self): 
         self.img = cv2.Canny(self.img, 50, 150)
+        self.canny = self.img
         self.roi()
 
     def roi(self): 
-        height = self.img.shape[0]
-        width = self.img.shape[1]
-        polygons = np.array([
-        [(0, height), (width, height), (width//2, height//2)]
-        ])
+        rows, cols = self.img.shape[:2]
         self.mask = np.zeros_like(self.img)
-        cv2.fillPoly(self.mask, polygons, 255)
+        
+        left_bottom = [cols * 0.1, rows]
+        right_bottom = [cols * 0.95, rows]
+        left_top = [cols * 0.4, rows * 0.6]
+        right_top = [cols * 0.6, rows * 0.6]
+        
+        vertices = np.array([[left_bottom, left_top, right_top, right_bottom]], dtype=np.int32)
+        
+        if len(self.mask.shape) == 2:
+            cv2.fillPoly(self.mask, vertices, 255)
+        else:
+            cv2.fillPoly(self.mask, vertices, (255, ) * self.mask.shape[2])
         self.img = cv2.bitwise_and(self.img, self.mask)
         self.hough()
     
     def hough(self): 
-        self.lines = cv2.HoughLinesP(self.img, 2, np.pi/180, 100, np.array([]), minLineLength=10, maxLineGap=5 )
-        self.average_slope()
+        self.lines = cv2.HoughLinesP(self.img, 1, np.pi/180, 20, np.array([]), minLineLength=20, maxLineGap=300)
+        if len(self.lines) > 0: 
+            self.average_slope()
 
     def make_coordinates(self, line_parameters):
         slope, intercept = line_parameters
@@ -61,17 +95,21 @@ class LaneDetection:
 
     def draw_lines(self): 
         self.line_image = np.zeros_like(self.img)
-        if self.lines is not None:
-            for x1, y1, x2, y2 in self.lines[0]:
-                cv2.line(self.line_image, (x1,y1), (x2,y2), (255, 0, 0), 10)
+
+        if self.avg_lines is not None:
+            for x1, x2, y1, y2 in self.avg_lines:
+                cv2.line(self.line_image, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 10)
     
     def show_lanes(self): 
-        print(self.avg_lines)
         plt.imshow(self.line_image)
     
     def show_final(self): 
-        cv2.imshow("result", cv2.addWeighted(self.img, 0.8, self.line_image, 1, 1))
+        line = self.line_image[..., np.newaxis]
+        return self.original | line
 
     def show_mask(self): 
         plt.imshow(self.mask)
 
+    
+    def show_canny(self): 
+        return self.canny
